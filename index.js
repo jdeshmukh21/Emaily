57cd98361d77d15fced8e0e4151cef98034740ad
jdeshmukh21 <jmdeshmukh21@gmail.com>
2025-10-25 12:17:54 +0530

    Clean version without secrets

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const Keys = require("./config/keys");
+mongoose.connect(require("./config/keys").mongoURI);
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [Keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./models/User");
+
+require("./services/passport");
+
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
